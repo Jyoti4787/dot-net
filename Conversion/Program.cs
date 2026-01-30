fe6307df2765769ac8362a7506abc4922078c7f8
vairagi-tech <abhaykumaraks7226@email.com>
2026-01-30 12:15:54 +0530

    commit

--- Conversion/Program.cs
+++ Conversion/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Solution
+{
+    public static double FeetToCentimeters(int feet)
+    {
+        double cm = feet * 30.48;
+        return Math.Round(cm, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static void Main(string[] args)
+    {
+        int feet = Convert.ToInt32(Console.ReadLine());
+        double result = FeetToCentimeters(feet);
+        Console.WriteLine(result);
+    }
+}
