69b2951dc791a4fbfed9e2e03d0179efb666ef8b
vairagi-tech <abhaykumaraks7226@email.com>
2026-01-30 11:31:28 +0530

    j

---
+++ DisplayHeight/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Solution
+{
+    public static string GetHeightCategory(int heightCm)
+    {
+        if (heightCm < 150)
+            return "Short";
+        else if (heightCm < 180)
+            return "Average";
+        else
+            return "Tall";
+    }
+
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int heightCm))
+        {
+            string category = GetHeightCategory(heightCm);
+            Console.WriteLine(category);
+        }
+    }
+}
--- topbrains/Program.cs
+++ topbrains/Program.cs
@@ -1,31 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//Custom Sorting
 
-namespace CustomSortingExample
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<Student> students = new List<Student>
-            {
-                new Student { Name = "Jyoti", Age = 20, Marks = 85 },
-                new Student { Name = "Deepak", Age = 18, Marks = 85 },
-                new Student { Name = "Shivansh", Age = 22, Marks = 90 },
-                new Student { Name = "Srikaran", Age = 19, Marks = 90 },
-                new Student { Name = "Aditya", Age = 23, Marks = 91 }
-            };
-            students.Sort(new StudentComparer());
 
-            Console.WriteLine("Sorted Students:");
-            foreach (var student in students)
-            {
-                Console.WriteLine(
-                    $"Name: {student.Name}, Age: {student.Age}, Marks: {student.Marks}"
-                );
-            }
+// using System;
+// using System.Collections.Generic;
 
-            Console.ReadLine();
-        }
-    }
-}
+// namespace CustomSortingExample
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             List<Student> students = new List<Student>
+//             {
+//                 new Student { Name = "Jyoti", Age = 20, Marks = 85 },
+//                 new Student { Name = "Deepak", Age = 18, Marks = 85 },
+//                 new Student { Name = "Shivansh", Age = 22, Marks = 90 },
+//                 new Student { Name = "Srikaran", Age = 19, Marks = 90 },
+//                 new Student { Name = "Aditya", Age = 23, Marks = 91 }
+//             };
+//             students.Sort(new StudentComparer());
+
+//             Console.WriteLine("Sorted Students:");
+//             foreach (var student in students)
+//             {
+//                 Console.WriteLine(
+//                     $"Name: {student.Name}, Age: {student.Age}, Marks: {student.Marks}"
+//                 );
+//             }
+
+//             Console.ReadLine();
+//         }
+//     }
+// }
