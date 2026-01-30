38514dc5ec05a743047f100e4a161af319c1ab5f
vairagi-tech <abhaykumaraks7226@email.com>
2026-01-30 11:58:51 +0530

    commit

--- MidPointRounding/Program.cs
+++ MidPointRounding/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Solution
+{
+    public static double GetCircleArea(double radius)
+    {
+        double area = Math.PI * radius * radius;
+        return Math.Round(area, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static void Main(string[] args)
+    {
+        string? input = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Invalid Input");
+            return;
+        }
+
+        input = input.Trim();
+        if (double.TryParse(input, out double radius) && radius >= 0)
+        {
+            double area = GetCircleArea(radius);
+            Console.WriteLine(area.ToString("F2"));
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input");
+        }
+    }
+}
