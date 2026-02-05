d401806e888ab7f6485852a8f6034fafe4f3a993
vairagi-tech <abhaykumaraks7226@email.com>
2026-02-05 15:35:53 +0530

    commit

---
+++ GuessGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random r = new Random();
+        int number = r.Next(1, 11);
+        int chances = 3;
+        int money = 0;
+
+        Console.WriteLine("Number Guess Game");
+        Console.WriteLine("Guess a number between 1 and 10");
+        Console.WriteLine("You have 3 chances");
+
+        while (chances > 0)
+        {
+            Console.Write("Enter number: ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess == number)
+            {
+                money = 1000;
+                Console.WriteLine("Correct guess");
+                Console.WriteLine("You won $" + money);
+                break;
+            }
+            else
+            {
+                chances--;
+                if (chances > 0)
+                {
+                    Console.WriteLine("Wrong guess");
+                    Console.WriteLine("Chances left: " + chances);
+                }
+                else
+                {
+                    Console.WriteLine("Game over");
+                    Console.WriteLine("Correct number was: " + number);
+                }
+            }
+        }
+    }
+}
---
+++ NullValue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Solution
+{
+    public static double? AverageNonNull(double?[] values)
+    {
+        double sum = 0;
+        int count = 0;
+
+        foreach (var v in values)
+        {
+            if (v.HasValue)
+            {
+                sum += v.Value;
+                count++;
+            }
+        }
+
+        if (count == 0)
+            return null;
+
+        double avg = sum / count;
+        return Math.Round(avg, 2, MidpointRounding.AwayFromZero);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double?[] arr = { 10.0, null, 20.0, 30.0 };
+
+        var result = Solution.AverageNonNull(arr);
+
+        if (result.HasValue)
+            Console.WriteLine(result.Value);
+        else
+            Console.WriteLine("null");
+    }
+}
